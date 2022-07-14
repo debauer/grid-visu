b73859c9286a7f4c2ae8aca06df1b64b09ce6948
David Bauer <bauer@synyx.de>
2022-07-14 12:37:39 +0200

    lala

--- src/display.py
+++ src/display.py
@@ -5,7 +5,7 @@ from pygame.surface import SurfaceType
 from pygame.time import get_ticks
 
 from grid.base import GridBox, get_grid, create_grid
-from grid.content import ContentImage
+from grid.content import ContentImage, ContentValue
 from misc.types import GridLayout, GridPosition
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, grid_layout
 
@@ -13,11 +13,14 @@ pygame.display.init()
 pygame.font.init()
 surface: SurfaceType = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.DOUBLEBUF)
 create_grid(surface, grid_layout)
+
 GridBox(GridPosition(0, 0), GridLayout(4, 3), "audio")
 camera_box = GridBox(GridPosition(6, 0), GridLayout(6, 3), "camera", )
-GridBox(GridPosition(0, 3), GridLayout(1, 1), "1")
+co2_box = GridBox(GridPosition(0, 3), GridLayout(1, 1), "co2")
 GridBox(GridPosition(1, 3), GridLayout(1, 1), "2")
 
+co2value = ContentValue(co2_box,)
+co2value.set_value(499.2)
 c1image = ContentImage(camera_box)
 c2image = ContentImage(camera_box)
 c1image.load_image("img/example.jpg")
@@ -31,6 +34,7 @@ ticks = get_ticks()
 perf = time.perf_counter()
 
 while True:
+    co2value.draw()
     if lala:
         lala = False
         c1image.draw()
@@ -45,3 +49,4 @@ while True:
     print(f"perf: {p - perf}")
     ticks = t
     perf = p
+    time.sleep(1)
--- src/grid/content.py
+++ src/grid/content.py
@@ -4,7 +4,7 @@ import pygame
 from pygame.surface import SurfaceType
 from grid.base import get_grid, GridBoxBase
 from misc.font import text_font
-from misc.types import GridPosition, GridLayout, Coordinate, Size
+from misc.types import GridPosition, GridLayout, Coordinate, Size, Color
 from settings import content_color, grid_layout
 
 
@@ -38,7 +38,6 @@ class ContentBase:
 
 
 class ContentImage(ContentBase):
-
     def __init__(self, box: GridBoxBase, name: str = ""):
         super().__init__(box, name)
         self.img = None
@@ -51,6 +50,38 @@ class ContentImage(ContentBase):
         if self.img:
             self.surface.blit(self.img, (self.coordinates.x, self.coordinates.y))
 
+
+class ContentValue(ContentBase):
+    def __init__(self, box: GridBoxBase, name: str = ""):
+        super().__init__(box, name)
+        self.threshold: dict[float, Color] = {}
+        self.value = 0.0
+
+    def set_threshold(self, value: float, color: Color):
+        self.threshold[value] = color
+        
+    def set_value(self, value: float):
+        self.value = value
+
+    def draw(self):
+        c1 = Coordinate(self.coordinates.x, self.coordinates.y)
+        c2 = Coordinate(
+            self.coordinates.x, self.coordinates.y + self.size.y
+        )
+        c3 = Coordinate(
+            self.coordinates.x + self.size.x,
+            self.coordinates.y + self.size.y,
+        )
+        c4 = Coordinate(
+            self.coordinates.x + self.size.x, self.coordinates.y
+        )
+        pygame.draw.polygon(self.surface, (123,0,0), (c1, c2, c3, c4))
+        text = text_font().render(str(self.value), True, content_color.text)
+        text_rect = text.get_rect(center=(self.coordinates.x + self.size.x/2, self.coordinates.y + self.size.y/2))
+        self.surface.blit(
+            text,
+            text_rect,
+        )
 
 class ContentList(ContentBase):
 
